32ff9d254992faf6a577291230f1cbc51c716bcf
fernando <fernandocmar@gmail.com>
2020-07-16 19:59:39 -0300

    -

---
+++ vies-vat-validator/src/main/java/com/martins/VatValidation.java
@@ -0,0 +1,31 @@
+package com.martins;
+
+import io.micronaut.core.annotation.Introspected;
+
+import java.io.Serializable;
+
+@Introspected
+public class VatValidation extends VatValidationRequest implements Serializable {
+    private Boolean valid;
+
+    public VatValidation() {
+
+    }
+
+    public VatValidation(String memberStateCode, String vatNumber, Boolean valid) {
+        super(memberStateCode, vatNumber);
+        this.valid = valid;
+    }
+
+    public Boolean getValid() {
+        return valid;
+    }
+
+    public void setValid(Boolean valid) {
+        this.valid = valid;
+    }
+
+    public Boolean isValid() {
+        return valid;
+    }
+}
